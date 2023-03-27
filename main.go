e740c52c527d1be579121542c9ca2dbd351b768d
Hamed Nazari <hamed.nazari70@gmail.com>
2023-03-27 01:20:05 -0600

    add logic for convert to JSON

--- main.go
+++ main.go
@@ -1,34 +1,77 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"net/http"
+	"os"
+	"time"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	dto "github.com/prometheus/client_model/go"
+
+	"github.com/prometheus/prom2json"
 )
 
 func main() {
-	// Create a new HTTP server to listen for requests
-	http.Handle("/metrics", promhttp.Handler())
+	cert := flag.String("cert", "", "client certificate file")
+	key := flag.String("key", "", "client certificate's key file")
+	skipServerCertCheck := flag.Bool("accept-invalid-cert", false, "Accept any certificate during TLS handshake. Insecure, use only for testing.")
+	flag.Parse()
 
-	// Start the server on port 8080
-	http.ListenAndServe(":8080", nil)
+	var urlString string = "http://localhost:8081/metrics"
+
+	mfChan := make(chan *dto.MetricFamily, 1024)
+
+	transport, err := makeTransport(*cert, *key, *skipServerCertCheck)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	go func() {
+		err := prom2json.FetchMetricFamilies(urlString, mfChan, transport)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}()
+
+	result := []*prom2json.Family{}
+	for mf := range mfChan {
+		result = append(result, prom2json.NewFamily(mf))
+	}
+	jsonText, err := json.Marshal(result)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "error marshaling JSON:", err)
+		os.Exit(1)
+	}
+	if _, err := os.Stdout.Write(jsonText); err != nil {
+		fmt.Fprintln(os.Stderr, "error writing to stdout:", err)
+		os.Exit(1)
+	}
+	fmt.Println()
 }
 
-// Handler function to extract and encode metrics as JSON
-func metricsHandler(w http.ResponseWriter, r *http.Request) {
-	// Get the metrics from the Prometheus server
-	metrics, err := promhttp.DefaultGatherer.Gather()
-	if err != nil {
-		http.Error(w, "Error gathering metrics", http.StatusInternalServerError)
-		return
+func makeTransport(
+	certificate string, key string,
+	skipServerCertCheck bool,
+) (*http.Transport, error) {
+	// Start with the DefaultTransport for sane defaults.
+	transport := http.DefaultTransport.(*http.Transport).Clone()
+	// Conservatively disable HTTP keep-alives as this program will only
+	// ever need a single HTTP request.
+	transport.DisableKeepAlives = true
+	// Timeout early if the server doesn't even return the headers.
+	transport.ResponseHeaderTimeout = time.Minute
+	tlsConfig := &tls.Config{InsecureSkipVerify: skipServerCertCheck}
+	if certificate != "" && key != "" {
+		cert, err := tls.LoadX509KeyPair(certificate, key)
+		if err != nil {
+			return nil, err
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
 	}
-
-	// Encode the metrics as JSON and send them in the response
-	encoder := json.NewEncoder(w)
-	err = encoder.Encode(metrics)
-	if err != nil {
-		http.Error(w, "Error encoding metrics as JSON", http.StatusInternalServerError)
-		return
-	}
+	transport.TLSClientConfig = tlsConfig
+	return transport, nil
 }
