d36e6c35dadfa3a2385232127c721ec80206d283
Hamed Nazari <hamed.nazari70@gmail.com>
2023-04-05 18:13:33 -0600

    list of addresses

--- main.go
+++ main.go
@@ -20,37 +20,46 @@ func main() {
 	skipServerCertCheck := flag.Bool("accept-invalid-cert", false, "Accept any certificate during TLS handshake. Insecure, use only for testing.")
 	flag.Parse()
 
-	var urlString string = "http://localhost:8081/metrics"
+	// var urlString string = "http://localhost:8081/metrics"
+	stringURLs := []string{"localhost", "localhost"}
 
-	mfChan := make(chan *dto.MetricFamily, 1024)
+	for _, url := range stringURLs {
 
-	transport, err := makeTransport(*cert, *key, *skipServerCertCheck)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	go func() {
-		err := prom2json.FetchMetricFamilies(urlString, mfChan, transport)
+		fmt.Println("http://" + url + ":3000/metrics")
+
+		var urlString string = "http://" + url + ":3000/metrics"
+
+		mfChan := make(chan *dto.MetricFamily, 1024)
+
+		transport, err := makeTransport(*cert, *key, *skipServerCertCheck)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			fmt.Fprintln(os.Stderr, "error writing to stdout:", err)
 			os.Exit(1)
 		}
-	}()
+		go func() {
+			err := prom2json.FetchMetricFamilies(urlString, mfChan, transport)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}()
 
-	result := []*prom2json.Family{}
-	for mf := range mfChan {
-		result = append(result, prom2json.NewFamily(mf))
+		result := []*prom2json.Family{}
+		for mf := range mfChan {
+			result = append(result, prom2json.NewFamily(mf))
+		}
+		jsonText, err := json.Marshal(result)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "error marshaling JSON:", err)
+			os.Exit(1)
+		}
+		if _, err := os.Stdout.Write(jsonText); err != nil {
+			fmt.Fprintln(os.Stderr, "error writing to stdout:", err)
+			os.Exit(1)
+		}
+		fmt.Println()
 	}
-	jsonText, err := json.Marshal(result)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "error marshaling JSON:", err)
-		os.Exit(1)
-	}
-	if _, err := os.Stdout.Write(jsonText); err != nil {
-		fmt.Fprintln(os.Stderr, "error writing to stdout:", err)
-		os.Exit(1)
-	}
-	fmt.Println()
+
 }
 
 func makeTransport(
